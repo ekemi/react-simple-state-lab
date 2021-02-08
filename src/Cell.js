17a9f2a6fb5e0cc847910c4aa3d133f0502db9f8
Yapnounkie <edmundopierre@yahoo.com>
2021-02-08 13:48:01 -0800

    fix the minor issues

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+
+
+class Cell extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    updateHandler = ()=> {
+        
+        this.setState({
+             color: '#333'    
+        })
+    }
+    render() {
+        return(<div className="cell" style={{backgroundColor:this.state.color}} onClick={this.updateHandler} ></div>)
+    }
+}
+
+export default Cell;
